f0d6c5a441aa8acf731e49e4f8e974c20b9a7ab8
Mykle <25211804+mykle1@users.noreply.github.com>
2019-11-24 15:44:30 -0500

    Add files via upload

--- MMM-ExtinctionCounter.js
+++ MMM-ExtinctionCounter.js
@@ -26,19 +26,19 @@ Module.register("MMM-ExtinctionCounter",{
 		iframe.scrolling = "no";
 
 		if (this.config.size === "large") {
-			iframe.height = "305px";
+			iframe.height = "105px";
 			iframe.width = "355px";
 		  type="text/javascript";
       iframe.src="https://www.theworldcounts.com/counters/degradation_and_destruction_of_ecosystems/species_extinction_facts/embed?background_color=black&color=" + this.config.textColor + "&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=24&token=221fff62ddf146b6ccf3e2eb0a3df0553ffd66fc";
 
   } else if (this.config.size === "medium") {
-		iframe.height = "205px";
+		iframe.height = "105px";
 		iframe.width = "305px";
 		type="text/javascript";
 		iframe.src="https://www.theworldcounts.com/counters/degradation_and_destruction_of_ecosystems/species_extinction_facts/embed?background_color=black&color=" + this.config.textColor + "&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=20&token=221fff62ddf146b6ccf3e2eb0a3df0553ffd66fc";
 
 	} else if (this.config.size === "small") {
-		iframe.height = "105px";
+		iframe.height = "75px";
 		iframe.width = "245px";
 		type="text/javascript";
 		iframe.src="https://www.theworldcounts.com/counters/degradation_and_destruction_of_ecosystems/species_extinction_facts/embed?background_color=black&color=" + this.config.textColor + "&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=16&token=221fff62ddf146b6ccf3e2eb0a3df0553ffd66fc";
